491740fcf6ff089d8999fc929bdba1a1fe33b92a
Mednoob <ahmadyaqdhan123@gmail.com>
2022-11-20 14:41:40 +0900

    feat(getPercentage): change result to object

--- modules/database/getPercentage.js
+++ modules/database/getPercentage.js
@@ -4,10 +4,13 @@ import { QueryTypes } from "sequelize";
 export async function getPercentage() {
     const res = await UserData.sequelize.query(`SELECT vote, round(
         COUNT(*) * 100.0 / (SELECT COUNT(*) FROM userdata WHERE vote is NOT NULL), 2
-    ) FROM userdata WHERE vote IS NOT NULL GROUP BY vote;
+    ) AS percentage FROM userdata WHERE vote IS NOT NULL GROUP BY vote;
     `, {
         type: QueryTypes.SELECT
     });
 
-    return res;
+    return res.reduce((p, c) => {
+        p[c.vote] = +c.percentage;
+        return p;
+    }, {});
 }
