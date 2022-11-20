3b76e65df4e4f894bc0d1df5cf6b05726e1e8966
Mednoob <ahmadyaqdhan123@gmail.com>
2022-11-20 14:40:14 +0900

    fix: lupa hapus validateUser di server/index.js

--- modules/server/index.js
+++ modules/server/index.js
@@ -9,7 +9,6 @@
  * - Body parser untuk express
  * - Library untuk mengambil file hasil upload contoh: multer
  */
-import { validateUser } from "../database/validateUser.js";
 import bodyParser from "body-parser";
 import express from "express";
 
@@ -19,13 +18,7 @@ const port = 3000;
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-app.post('/validate-user', async (req,res) => {
-    const vRes = await validateUser(req.body);
-    res.status(vRes.valid ? 200 : 400).json(vRes);
-});
-
 // To do: integrasi dengan fungsi getPercentage yang ada di file modules\database\getPercentage.js
-// Silahkan lihat endpoint /validate-user sebagai contoh
 app.get('/vote-percentage', async (req,res) => {
     const server_user_data = {
         nis: req.body.nis,
@@ -39,7 +32,6 @@ app.get('/vote-percentage', async (req,res) => {
 })
 
 // To do: integrasi dengan fungsi saveVote yang ada di file modules/database/saveVote.js
-// Silahkan lihat endpoint /validate-user sebagai contoh
 app.post('/vote', async (req, res) =>{
     const vote = {
         id: req.body.id,
