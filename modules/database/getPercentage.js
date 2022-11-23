ecaa84e1c8e2d2bf2b7287d9982bc5a6d6f20bbb
Mednoob <ahmadyaqdhan123@gmail.com>
2022-11-23 23:39:58 +0900

    feat(getPercentage): change to OSIS percentage

--- modules/database/getPercentage.js
+++ modules/database/getPercentage.js
@@ -2,16 +2,16 @@ import { UserData } from "../../models/UserData.js";
 import { QueryTypes } from "sequelize";
 
 export async function getPercentage() {
-    const res = await UserData.sequelize.query(`SELECT vote, COUNT(*) as 'count', round(
-        COUNT(*) * 100.0 / (SELECT COUNT(*) FROM userdata WHERE vote is NOT NULL), 2
-    ) AS percentage FROM userdata WHERE vote IS NOT NULL GROUP BY vote;
+    const res = await UserData.sequelize.query(`SELECT osisvote AS vote, COUNT(*) as 'count', round(
+        COUNT(*) * 100.0 / (SELECT COUNT(*) FROM userdata WHERE osisvote is NOT NULL), 2
+    ) AS percentage FROM userdata WHERE osisvote IS NOT NULL GROUP BY osisvote;
     `, {
         type: QueryTypes.SELECT
     });
 
     return res.reduce((p, c) => {
         p[c.vote] = {
-            persentase: `${c.percentage}%`,
+            percentage: +c.percentage,
             total: +c.count
         };
         return p;
